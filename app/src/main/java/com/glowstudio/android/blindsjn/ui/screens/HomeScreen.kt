f18ae089aafafc5313fe363a2184fa4485962ed8
gyshim <gyshim1004@gmail.com>
2024-12-02 17:40:17 +0900

    Main Screen generate

--- app/src/main/java/com/glowstudio/android/blindsjn/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/glowstudio/android/blindsjn/ui/screens/HomeScreen.kt
@@ -1,5 +1,8 @@
 package com.glowstudio.android.blindsjn.ui.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.*
 import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
